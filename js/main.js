e7993a4edb1a43959966e9e98b755b78e0025115
jahyuofficial-debug <jahyuofficial@gmail.com>
2026-06-21 19:55:19 +0800

    Revert "feat: Design详情页Hero区域WebGL绿色不规则浮动线条Shader背景"
    This reverts commit 948c3e744aac976e58a91a54615f50e2d1c671c2.

--- js/main.js
+++ js/main.js
@@ -788,9 +788,6 @@ function bindMsgInteractions() {
 
 function navigateTo(pageName) {
     if (subPageContainer) {
-        if (typeof DesignShader !== 'undefined') {
-            DesignShader.destroy();
-        }
         subPageContainer.remove();
         subPageContainer = null;
     }
@@ -1395,12 +1392,6 @@ function navigateToDesignWorkDetail(id) {
     subPageContainer.innerHTML = DesignPage.buildDetail(id);
     app.appendChild(subPageContainer);
     currentPage = 'design-work-detail';
-
-    // Init WebGL shader background
-    var hero = document.querySelector('.dw-detail-hero');
-    if (hero && typeof DesignShader !== 'undefined') {
-        DesignShader.init(hero);
-    }
     updateNavActiveState(currentPage);
 
     var backBtn = document.getElementById('dwDetailBack');
