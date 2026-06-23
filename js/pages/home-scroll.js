fbec1a328cb67e4a3a16fc323c98833a0582a10e
jahyuofficial-debug <jahyuofficial@gmail.com>
2026-06-24 02:37:17 +0800

    feat: add Google icon, fix Instagram SVG to camera outline

--- js/pages/home-scroll.js
+++ js/pages/home-scroll.js
@@ -23,9 +23,16 @@
           '</div>' +
         '</div>' +
         '<div class="social-qr-item" data-platform="instagram">' +
-          '<svg viewBox="0 0 24 24"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><circle cx="12" cy="12" r="5"/><circle cx="17.5" cy="6.5" r="1.5"/></svg>' +
+          '<svg viewBox="0 0 24 24"><path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 0 2.5 1.25 1.25 0 0 1 0-2.5M12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"/></svg>' +
           '<div class="social-qr-popup">' +
             '<img src="images/qr-instagram.png" alt="Instagram QR" onerror="this.style.display=\'none\';this.nextElementSibling.style.display=\'block\';">' +
+            '<span class="social-qr-popup-label" style="display:none;">QR coming soon</span>' +
+          '</div>' +
+        '</div>' +
+        '<div class="social-qr-item" data-platform="google">' +
+          '<svg viewBox="0 0 24 24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>' +
+          '<div class="social-qr-popup">' +
+            '<img src="images/qr-google.png" alt="Google QR" onerror="this.style.display=\'none\';this.nextElementSibling.style.display=\'block\';">' +
             '<span class="social-qr-popup-label" style="display:none;">QR coming soon</span>' +
           '</div>' +
         '</div>' +
